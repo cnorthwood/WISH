4368575ffc5554b85a40c882668bb331aa90b160
Chris Northwood <cnorthwood@gmail.com>
2009-07-09 23:25:26 +0000

    Add some command skeletons

--- p10/parser.py
+++ p10/parser.py
@@ -2,8 +2,47 @@
 
 class parser():
     
+    _handlers = dict()
+    
     def registerHandler(self, token, handler):
-        return
+        self._handlers[token] = handler
     
     def parse(self, string):
-        return
+        
+        # The standard requires we only accept strings ending in \r\n or \n
+        if (string[-1] != "\n"):
+            raise ParseError('Line endings were not as expected', string)
+        
+        if (len(string)) > 512:
+            raise ParseError('Line too long to be valid', string)
+        string = string.rstrip()
+        
+        high_level_parts = string.split(None, 2)
+        origin = high_level_parts[0]
+        command = high_level_parts[1]
+        if not command.isupper():
+            raise ParseError('Command not in uppercase', string)
+        params = high_level_parts[2]
+        if params[0] == ":":
+            params = [params[1:]]
+        else:
+            params = params.split(" :", 1)
+            if len(params) == 1:
+                last_arg = None
+            else:
+                last_arg = params[1]
+            params = params[0].split(None)
+            if last_arg != None:
+                params.append(last_arg)
+        self._handlers[command].handle(origin, params)
+
+class ParseError(Exception):
+    
+    line = ""
+    
+    def __init__(self, value, line):
+        self.value = value
+        self.line = line
+    
+    def __str__(self):
+        return repr(self.value) + " on line " + self.line
--- test/p10/parser.py
+++ test/p10/parser.py
@@ -7,17 +7,81 @@ class CommandHandlerDouble():
     
     rcvd = []
     
-    def handle(origin, line):
-        rcvd = line
+    def handle(self, origin, line):
+        self.rcvd = line
 
 class P10ParserTest(unittest.TestCase):
     
-    def testParseSimpleLine(self):
+    def testParseSimpleLineSingleArg(self):
         p = p10.parser.parser()
         d = CommandHandlerDouble()
         p.registerHandler("TEST", d)
         p.parse(":testuser TEST foo\r\n")
         self.assertEquals(['foo'], d.rcvd)
+    
+    def testParseSimpleLineTwoArg(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        p.parse(":testuser TEST foo bar\r\n")
+        self.assertEquals(['foo','bar'], d.rcvd)
+    
+    def testAcceptJustNewLine(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        p.parse(":testuser TEST foo\n")
+        self.assertEquals(['foo'], d.rcvd)
+        
+    def testRejectBadLineEndings(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        self.assertRaises(p10.parser.ParseError, p.parse, ":testuser TEST foo")
+    
+    def testLongArg(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        p.parse(":testuser TEST :foo bar\r\n")
+        self.assertEquals(['foo bar'], d.rcvd)
+    
+    def testLongArgWithShort(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        p.parse(":testuser TEST baz :foo bar\r\n")
+        self.assertEquals(['baz', 'foo bar'], d.rcvd)
+    
+    def testProtectAgainstLongArgs(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        p.parse(":testuser TEST b:az :foo bar\r\n")
+        self.assertEquals(['b:az', 'foo bar'], d.rcvd)
+    
+    def testRejectUnknownCommands(self):
+        p = p10.parser.parser()
+        self.assertRaises(p10.parser.ParseError, p.parse, ":testuser TEST foo")
+    
+    def testRejectLongLine(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        self.assertRaises(p10.parser.ParseError, p.parse, ":testuser TEST baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar baz foo bar\r\n")
+    
+    def testParseFirstLongArg(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        p.parse(":testuser TEST baz :foo bar: bar bar foo\r\n")
+        self.assertEquals(['baz', 'foo bar: bar bar foo'], d.rcvd)
+    
+    def testRejectLowercaseCommand(self):
+        p = p10.parser.parser()
+        d = CommandHandlerDouble()
+        p.registerHandler("TEST", d)
+        self.assertRaises(p10.parser.ParseError, p.parse, ":testuser test foo\r\n")
 
 def main():
     unittest.main()

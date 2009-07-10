70ea7541fe814234923591f030dafbefc564fc20
Chris Northwood <cnorthwood@gmail.com>
2009-07-10 18:32:56 +0000

    runtest.py should now hopefully exit with the correct value so Buildbot gets the right result

--- runtest.py
+++ runtest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import unittest
 import test.p10.parser
 import test.p10.base64
@@ -8,7 +9,11 @@ def main():
     suite = unittest.TestSuite()
     suite.addTest(unittest.TestLoader().loadTestsFromModule(test.p10.parser))
     suite.addTest(unittest.TestLoader().loadTestsFromModule(test.p10.base64))
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    if result.wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()

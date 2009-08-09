bcc30178c2f68bd604bc8a1d6fa8dcd9de9c78db
Chris Northwood <cnorthwood@gmail.com>
2009-08-09 00:01:02 +0000

    SERVER parser

--- p10/commands/server.py
+++ p10/commands/server.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python
 
+import genericcommand
+import p10.base64
+
+class server(genericcommand.genericcommand):
+    """ Parses servers being introduced """
+    
+    def handle(self, origin, args):
+        numeric = p10.base64.toInt(args[5][0:2])
+        name = args[0]
+        maxclients = p10.base64.toInt(args[5][2:5])
+        boot_ts = int(args[2])
+        link_ts = int(args[3])
+        protocol = args[4]
+        hops = int(args[1])
+        modes = []
+        flag = "+"
+        for mode in args[6]:
+            if mode == "+" or mode == "-":
+                flag = mode
+            else:
+                modes.append((flag + mode, None))
+        desc = args[-1]
+        self._state.newServer(origin, numeric, name, maxclients, boot_ts, link_ts, protocol, hops, modes, desc)
--- test/p10/commands/server.py
+++ test/p10/commands/server.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python
 
+import unittest
+import p10.commands.server
+
+class StateDouble:
+    server = None
+    def __init__(self):
+        self.server = None
+    def newServer(self, origin, numeric, name, maxclient, boot_ts, link_ts, protocol, hops, flags, description):
+        self.server = (origin, numeric, name, maxclient, boot_ts, link_ts, protocol, hops, flags, description)
+
+class ServerTest(unittest.TestCase):
+    
+    def testNewServer(self):
+        s = StateDouble()
+        c = p10.commands.server.server(s)
+        c.handle((1,None), ["test.example.com", "2", "1234", "1234", "P10", "ADAAB", "+gh", "A new server introduced"])
+        self.assertEquals(((1, None), 3, "test.example.com", 1, 1234, 1234, "P10", 2, [("+g", None), ("+h", None)], "A new server introduced"), s.server)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()

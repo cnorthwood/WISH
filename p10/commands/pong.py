39a498c378a25c501c7cf556b554ed12410593fe
Chris Northwood <cnorthwood@gmail.com>
2009-08-26 19:54:41 +0000

    Handle pongs

--- p10/commands/pong.py
+++ p10/commands/pong.py
@@ -13,7 +13,7 @@ class pong(genericcommand.genericcommand):
         genericcommand.genericcommand.__init__(self, state)
     
     def handle(self, origin, args):
-        if p10.base64.parseNumeric(args[1], self._state.maxClientNumerics) == (self._connection.numeric, None):
+        if p10.base64.parseNumeric(args[1], self._state.maxClientNumerics) == (self._state.getServerID(), None):
             self._connection.registerPong()
         else:
             self._state.registerPong(origin, args[0], args[1])

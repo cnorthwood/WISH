b1a8d955f7d224a53fa12b35c99fd196360972bb
Chris Northwood <cnorthwood@gmail.com>
2010-02-05 18:53:39 +0000

    NEw attempt to get the simple services to join a channel

--- services/worldirc.py
+++ services/worldirc.py
@@ -7,9 +7,11 @@ class worldirc:
     def __init__(self, state):
         self._state = state
         self._state.newUser((self._state.getServerID(), None), (self._state.getServerID(), 1), "W", "wish", "worldirc.org", [('+k', None)], 0, 0, 0, "The WorldIRC Test Bot")
-        self._state.registerCallback(self._state.CALLBACK_CHANNELCREATE, self.callbackChannelCreate)
+        self._state.createChannel((self._state.getServerID(), 1), "#help", self._state.ts())
+        self._state.createChannel((self._state.getServerID(), 1), "#opers", self._state.ts())
     
     def callbackChannelCreate(self, (origin, name, ts)):
+        self._state.privmsg((self._state.getServerID(), 1), "#opers", "Hello, I am the test bot for the next-generation WorldIRC services. I just got channel creation info about " + name)
         if name == "#help" or name == "#opers":
-            self._state.joinChannel((self._state.getServerID(), 1), (self._state.getServerID(), 1), name, "ov", self._state.ts())
+            
             self._state.privmsg((self._state.getServerID(), 1), name, "Hello, I am the test bot for the next-generation WorldIRC services.")

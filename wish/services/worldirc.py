786b3b809459e2371a9d7b0d15bde9a49eb55ef7
Chris Northwood <chris@pling.org.uk>
2011-12-30 21:00:39 +0000

    Update WBot interface

--- wish/services/worldirc.py
+++ wish/services/worldirc.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 
-class worldirc:
+class WBot():
     
     _state = None
     
     def __init__(self, state):
         self._state = state
-        self._state.newUser((self._state.server_id, None), (self._state.server_id, 1), "W", "wish", "worldirc.org", [('+k', None)], 0, 0, 0, "The WorldIRC Test Bot")
-        self._state.createChannel((self._state.server_id, 1), "#help", self._state.ts())
-        self._state.createChannel((self._state.server_id, 1), "#opers", self._state.ts())
+        self._state.new_user(
+            (self._state.server_id, None),
+            (self._state.server_id, 1),
+            "W", "wish", "worldirc.org",
+            [('+k', None)], 0, 0, 0, "The WorldIRC Test Bot"
+        )
+        
+        self._state.create_channel(
+            (self._state.server_id, 1),
+            "#help",
+            self._state.ts()
+        )
+        
+        self._state.create_channel(
+            (self._state.server_id, 1),
+            "#opers", self._state.ts()
+        )
+        
+        self._state.register_callback(self._state.CALLBACK_CHANNELJOIN,
+                                      self.callback_channel_join)
     
-    def callbackChannelCreate(self, (origin, name, ts)):
-        self._state.privmsg((self._state.server_id, 1), "#opers", "Hello, I am the test bot for the next-generation WorldIRC services. I just got channel creation info about " + name)
-        if name == "#help" or name == "#opers":
-            
-            self._state.privmsg((self._state.server_id, 1), name, "Hello, I am the test bot for the next-generation WorldIRC services.")
+    def callback_channel_join(self, origin, name, ts):
+        if name in ["#help", "#opers"]:
+            self._state.privmsg(
+                (self._state.server_id, 1),
+                name,
+                "Hello, I am the test bot for the next-generation WorldIRC services."
+            )
